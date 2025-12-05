88966dc5982e93e7c45580e4795f3b9eef65c1a8
Tiger <tiger.hu.liu@gmail.com>
2025-12-05 11:45:01 +0800

    fix cloudflare pages deploy error

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 const nextConfig = {
   output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [],
   },
   trailingSlash: true,
